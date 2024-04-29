326fe575af60f2358b501aefde58e30aab5b9ab6
pageadam <a.d.page@lancaster.ac.uk>
2024-04-29 16:29:11 +0100

    Added lead_time attribute to Customer class

--- Customers.py
+++ Customers.py
@@ -10,20 +10,34 @@ class Customer():
     Attributes
     ----------
     threshold : float
-        maximum price (£) customer is willing to pay 
+        maximum price (£) customer is willing to pay
+
+    lead_time : float
+        day(s) between booking and arriving at car park
     
 
     Methods
     -------
-    getThreshold():
+    getThreshold() -> float:
         Returns willingness to pay threshold of customer
 
-    setThreshold(threshold:float):
+    setThresholdExact(threshold: float):
         Changes the customer's threshold attribute to the value 'threshold'      
+    
+    setThresholdLinear(min_price: int = 0, max_price: int = 100):
+        Changes the customer's threshold attribute to a random variable generated from a linearly decreasing probability mass function
+
+    getLeadTime() -> int:
+        Returns customer's lead time
+    
+    setLeadTimeExact(lead_time: int):
+        Changes the customer's lead_time attribute to the value 'lead_time'
+
+    set LeadTimeUniform(min_lead: int = 1, max_lead: int = 7):
+        Changes the customer's lead_time attribute to a random variable generated from a disctrete Uniform[min_lead,max_lead] distribution
     """
 
-    def __init__(self, willingness_to_pay_threshold:float = float ):
-
+    def __init__(self, willingness_to_pay_threshold: float = float, lead_time: int = int):
         """
         Constructs all the necessary attributes for the customer object.
 
@@ -32,19 +46,23 @@ class Customer():
         willingness_to_pay_threshold : float, optional
             maximum price (£) customer is willing to pay
             converted to 'threshold' attribute (default is float)
+
+        lead_time : int, optional
+            days to go from customer booking to arriving at the car park (default is int)
         """
     
         self.__threshold = willingness_to_pay_threshold
+        self.__lead_time = lead_time
 
 
 
     def getThreshold(self) -> float:
-        """ Returns willingness to pay threshold of customer."""
-        return self.__threshold
+        """ Returns willingness to pay threshold of customer. """
+        return self.__threshold  
     
 
 
-    def setThresholdExact(self, threshold:float):
+    def setThresholdExact(self, threshold: float):
         """ 
         Changes the customer's threshold attribute to the value 'threshold'.
         
@@ -55,22 +73,24 @@ class Customer():
         """
         self.__threshold = threshold
 
-    def setThresholdLinear(self,min_price:float = 0, max_price:float = 100):
+
+
+    def setThresholdLinear(self, min_price: int = 0, max_price: int = 100):
         """
         Changes the customer's threshold attribute 
         according to a linearly decreasing probability mass function
 
         Parameters
         ----------
-        min_price : float, optional
+        min_price : int, optional
             minimum price from which customer's willingness to pay threshold will be generated from
             (default is 0)
 
-        max_price : float,optional
+        max_price : int,optional
             maximum price from which customer's willingness to pay threshold will be generated from
             (default is 100)
         """
-        #create range from minumu and maximum parameters
+        #create range from minumum and maximum parameters
         price_range = range(min_price,max_price+1)
 
         #create sum now to avoid repeating function
@@ -88,4 +108,36 @@ class Customer():
         #assign random threshold to attribute
         self.__threshold = float(threshold)
 
-    
+
+    def getLeadTime(self) -> int:
+        """ Returns customer's lead time. """
+        return self.__lead_time
+
+    def setLeadTimeExact(self, lead_time: int):
+        """ 
+        Changes the customer's lead_time attribute to the value 'lead_time'.
+        
+        Parameters
+        ----------
+        lead_time : int
+            number of days between customer booking adn then arriving in person
+        """
+        self.__lead_time = lead_time
+
+    def setLeadTimeUniform(self, min_lead: int = 1, max_lead: int = 7):
+        """
+        Changes the customer's lead_time attribute to a integer 
+        generated from a discrete Uniform[min_lead,max_lead] distribution
+
+        Parameters
+        ----------
+        min_lead : int
+            minumum number of days that can be assigned to a customer as lead time
+            (default is 1)
+
+        max_lead : int 
+            maximum number of days that can be assigned to a customer as lead time
+            (default is 7)
+        """
+
+        self.__lead_time = np.random.randint(low = min_lead, high = max_lead+1)
